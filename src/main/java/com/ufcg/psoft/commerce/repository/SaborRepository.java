9335b9f061395175d41bc9b974fe8ba275febac6
Cazans <caique.campelo@ccc.ufcg.edu.br>
2023-09-17 17:57:28 -0300

    remoção de metodo sem utilidade

--- src/main/java/com/ufcg/psoft/commerce/repository/SaborRepository.java
+++ src/main/java/com/ufcg/psoft/commerce/repository/SaborRepository.java
@@ -5,6 +5,4 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ufcg.psoft.commerce.model.Sabor;
 
-public interface SaborRepository extends JpaRepository<Sabor, Long> {
-  List<Sabor> findByEstabelecimentoId(Long id);
-}
+public interface SaborRepository extends JpaRepository<Sabor, Long> {}
