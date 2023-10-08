0f311a091c6826ab893804278a06cff2bca3c3e7
Bruno <brbosshd2@gmail.com>
2023-10-08 09:35:16 -0300

    feat: Notificação Disponibilidade

---
+++ src/main/java/com/ufcg/psoft/commerce/service/cliente/Notification.java
@@ -0,0 +1,7 @@
+package com.ufcg.psoft.commerce.service.cliente;
+
+import com.ufcg.psoft.commerce.model.Sabor;
+
+public interface Notification {
+    public void notificate(String sabor, String nomeCliente);
+}
